a75ff88e42dff6a64ce0a2ee935a71c1526165d7
dilip banjaraa <dileepbanjara595@gmail.com>
2026-08-25 00:45:07 +0530

    fix: remove hindi comment from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Vercel पर टाइप एरर को इग्नोर करने के लिए ताकि बिल्ड पास हो जाए
     ignoreBuildErrors: true,
   },
   eslint: {
